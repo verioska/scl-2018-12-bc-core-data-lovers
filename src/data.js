5b8f2d0efacff663c119e9b073877b51ad234491
Johanna Goenaga <johannagoenagac@gmail.com>
2019-01-08 11:45:10 -0300

    actualizando archivos

--- src/data.js
+++ src/data.js
@@ -1,23 +1,84 @@
-const datos= Object.values(LOL.data);
+const data= Object.values(LOL.data);
 
-function filterByRol(datos,condition) {
-     const categoria=datos.filter(tipos =>{
-     return tipos.tags.indexOf(condition)!==-1;
-   });
-  
- return categoria;
+function filterData(data,condition) {
+    const category=data.filter(tipos =>{
+    return tipos.tags.indexOf(condition)!==-1;
+  });
+  return category;
 }
  
-function ordenar(datos,order){
-  return ( order === "a-z" ? datos.sort() : datos.reverse() );
+function sortData(data,sortBy,sortOrder){
+  if(sortBy==="id" && sortOrder === "a-z" ){
+    data.sort((a,b)=>{
+      if(b.id>a.id){
+        return -1;
+      }
+      if(a.id>b.id){
+       return 1;
+      }
+      return 0;
+    });
+  }
+  if(sortBy==="id" && sortOrder === "z-a" ){
+    data.sort((a,b)=>{
+     if(a.id>b.id){
+        return -1;
+      }
+     if(b.id>a.id){
+       return 1;
+      }
+      return 0;
+    });
+  }
+  return data
 }
 
 function findChampion(id) {
   let championInfo;
-  for(let i=0; i<datos.length;i++){
-      if(datos[i].id===id){
-        championInfo=datos[i]
-      }
+  for(let i=0; i<data.length;i++){
+    if(data[i].id===id){
+     championInfo=data[i]
+    }
   }
   return championInfo;
+}
+
+function computeStats(datos) {
+  let tag=["Assassin","Fighter","Mage","Marksman","Support","Tank"];      
+  let list_tag=[];
+  
+  for (let m=0;m<tag.length; m++){
+      let name= tag[m];
+      let categoria=filterData(datos,name);
+      let p=0;
+      let list= [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];    
+      
+      for (let i=0;i<categoria.length; i++){ //en algun momento i será la cantidad maxima de campeones
+          
+          let champion=categoria[i] //objeto  
+          
+          const information1 = ((champion || {}).info); //objeto
+          const information= Object.entries(information1); //array
+          
+          const statics1=((champion || {}).stats); //objeto
+          const statics= Object.entries(statics1); //array   
+
+          for (let j=0;j<4;j++){
+              let n=information[j][1];
+              list[j]+=n;                         
+          }
+          for (let j=4;j<20;j++){
+              let n=statics[j][1];
+              list[j]+=n;            
+          }
+          p+=1;        
+      }
+      for (let j=0;j<list.length;j++){
+          let n= list[j]/p;        
+          let t=n.toFixed(2);
+          list[j]= t;      
+      }           
+      list_tag.push(list);
+  }
+  return list_tag;
 }
--- src/main.js
+++ src/main.js
@@ -13,72 +13,438 @@ window.onload =() =>{
     document.getElementById("page1").style.display="none";
     document.getElementById("page2").style.display="block";
     
-    showCards(datos);
+    showCards(data);
   })
-
-  function showCards(datos) {
+  function showCards(data) {
+    document.getElementById("page3").style.display="none";
     document.getElementById('championsList').innerHTML = '';
-    for (let i = 0; i < datos.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       document.getElementById('championsList').innerHTML += `
-      <div class="col s6 m3" >
-        <div class="card">
-          <img class="imagen-lol responsive-img" src="${datos[i].splash}" >
+      <div class="container" style="width: 120%">  
+      <div class="col s6 m3 l3" >
+          <div class="card">
+            <img class="imagen-lol responsive-img" src="${data[i].splash}" >
             <div class="card-content">
-              <span class="card-title activator grey-text text-darken-2"><h6>${datos[i].name}</h6><i class="material-icons right">more_vert</i></span>
+              <span class="card-title activator grey-text text-darken-2"><h6>${data[i].name}</h6><i class="material-icons right">more_vert</i></span>
             </div>
-          <div class="card-reveal">
-            <span class="card-title grey-text text-darken-4">${datos[i].name}<i class="material-icons right">close</i></span>
-            <p> Ataque: ${ datos[i].info.attack }</p>
-            <p> Defensa: ${ datos[i].info.defense }</p>
-            <p> Magia: ${ datos[i].info.magic }</p>
-            <p> Dificultad: ${ datos[i].info.difficulty }</p>
-            <span class="link" data-champion='${datos[i].id}'>Ver mas</span>
+            <div class="card-reveal">
+             <span class="card-title grey-text text-darken-4">${data[i].name}<i class="material-icons right">close</i></span>
+             <p> Ataque: ${ data[i].info.attack }</p>
+              <p> Defensa: ${ data[i].info.defense }</p>
+              <p> Magia: ${ data[i].info.magic }</p>
+              <p> Dificultad: ${ data[i].info.difficulty }</p>
+              <span class="link" data-champion='${data[i].id}'>Ver mas</span>
+            </div>
+          </div>
         </div>
-      </div>`  
-    }
-    const links = document.getElementsByClassName('link');
-    for (let i = 0; i < links.length; i++) {
-      links[i].addEventListener('click', (event) => {
-        event.preventDefault();
-        const id = event.srcElement.dataset.champion;
-        showDetail(id);
-      });
-    }
-  }
+        </div> ` 
+     }
+     const links = document.getElementsByClassName('link');
+     for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', (event) => {
+          event.preventDefault();
+          const id = event.srcElement.dataset.champion;
+         showDetail(id);
+        });
+       }
+   }
   function showDetail(id) {
     document.getElementById("page2").style.display="none";
     document.getElementById("page3").style.display="block";
     let championDetail = findChampion(id);
-    document.getElementById("championDetail").innerHTML += `
-  <div class="col s6 m3" >
-      <div class="card">
-        <img class="imagen-lol responsive-img" src="${championDetail.splash}" >
-          <div class="card-content">
-            <span class="card-title activator grey-text text-darken-2"><h6>${championDetail.name}</h6><i class="material-icons right">more_vert</i></span>
+    document.getElementById("championDetail").innerHTML = `
+      <div class="card large ">
+        <div>
+          <span class="link2" data-champion='${championDetail.id}'>X</span>
+          <img id="image-info" class="imagen-lol responsive-img" src="${championDetail.splash}">
+          <div id="champion-name">
+            <h3>${championDetail.name}</h3>
+            <p>${championDetail.partype}<p>
           </div>
-        <div class="card-reveal">
-          <span class="card-title grey-text text-darken-4">${championDetail.name}<i class="material-icons right">close</i></span>
-          <p> Ataque: ${ championDetail.info.attack }</p>
-          <p> Defensa: ${ championDetail.info.defense }</p>
-          <p> Magia: ${ championDetail.info.magic }</p>
-          <p> Dificultad: ${ championDetail.info.difficulty }</p>
-          <span class="link" data-champion='${championDetail.id}'>Ver mas</span>
-      </div>
-    </div>`  
-  }
- 
+         </div>
+       <section class="container" style="width:95%">
+          <div class="row" >
+            <div class="col l12">
+              <div class="row">
+                <div   class="col l3" >
+                  <div class="row">
+                   <div class="col l6">Attack</div>
+                    <div class="col l6" id="attack">${championDetail.info.attack}</div>
+                  </div>
+           </div>
+            <div class="col l3" > 
+              <div class="row">
+                <div class="col l6">MP</div>
+                <div class="col l6" id="mp">${championDetail.stats.mp}</div>
+              </div>
+            </div>
+            <div class="col l3" > 
+              <div class="row">
+                <div class="col l6">Spell block per level</div>
+                <div class="col l6" id="spellblockperlevel">${championDetail.stats.spellblockperlevel}</div>
+              </div>
+            </div>
+            <div class="col l3" > 
+              <div class="row">
+                <div class="col l6">Crit</div>
+                <div class="col l6" id="crit">${championDetail.stats.crit}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+              <div class="col l3">
+              <div class="row">
+                <div class="col l6">Defense</div>
+                <div class="col l6" id="defense">${championDetail.info.defense}</div>
+              </div>
+            </div>
+          <div class="col l3"> 
+            <div class="row">
+              <div class="col l6">MP per level</div>
+              <div class="col l6" id="mpperlevel">${championDetail.stats.mpperlevel}</div>
+              </div>
+            </div>
+            <div class="col l3"> 
+              <div class="row">
+                <div class="col l6">Attack range</div>
+                <div class="col l6" id="attackrange">${championDetail.stats.attackrange}</div>
+              </div>
+            </div>
+            <div class="col l3"> 
+              <div class="row">
+                <div class="col l6">Crit per level</div>
+                <div class="col l6" id="critperlevel">${championDetail.stats.critperlevel}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col l3" >
+              <div class="row">
+                <div class="col l6">Magic</div>
+                <div class="col l6" id="magic">${championDetail.info.magic}</div>
+              </div>
+            </div>
+            <div class="col l3" > 
+              <div class="row">
+                <div class="col l6">Movespeed</div>
+                <div class="col l6" id="movespeed">${championDetail.stats.movespeed}</div>
+              </div>
+            </div>
+            <div class="col l3" > 
+              <div class="row">
+                <div class="col l6">HP regen</div>
+                <div class="col l6" id="hpregen">${championDetail.stats.hpregen}</div>
+              </div>
+            </div>
+            <div class="col l3" > 
+              <div class="row">
+                <div class="col l6">Attack damage</div>
+                <div class="col l6" id="attackdamage">${championDetail.stats.attackdamage}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col l3">
+              <div class="row">
+                <div class="col l6">Difficulty</div>
+                <div class="col l6" id="difficulty">${championDetail.info.difficulty}</div>
+              </div>
+            </div>
+            <div class="col l3"> 
+              <div class="row">
+                <div class="col l6">Armor</div>
+                <div class="col l6" id="armor">${championDetail.stats.armor}</div>
+              </div>
+            </div>
+            <div class="col l3"> 
+              <div class="row">
+                <div class="col l6">HP Regen per level</div>
+                <div class="col l6" id="hpregenperlevel">${championDetail.stats.hpregenperlevel}</div>
+              </div>
+            </div>
+            <div class="col l3"> 
+              <div class="row">
+                <div class="col l6">Attack damage per level</div>
+                <div class="col l6" id="attackdamageperlevel">${championDetail.stats.attackdamageperlevel}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+              <div class="col l3" >
+              <div class="row">
+                <div class="col l6">HP</div>
+                <div class="col l6" id="hp">${championDetail.stats.hp}</div>
+              </div>
+            </div>
+          <div class="col l3" > 
+              <div class="row">
+                <div class="col l6">Armor per level</div>
+                <div class="col l6" id="armorperlevel">${championDetail.stats.armorperlevel}</div>
+              </div>
+            </div>
+          <div class="col l3" > 
+              <div class="row">
+                <div class="col l6">MP Regen per level</div>
+                <div class="col l6" id="mpregenperlevel">${championDetail.stats.mpregenperlevel}</div>
+              </div>
+            </div>
+            <div class="col l3" > 
+              <div class="row">
+                <div class="col l6">Attack speed offset</div>
+                <div class="col l6" id="attackspeedoffset">${championDetail.stats.attackspeedoffset}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+              
+            <div class="col l3">
+              <div class="row">
+                <div class="col l6">HP per level</div>
+                <div class="col l6" id="hpperlevel">${championDetail.stats.hpperlevel}</div>
+              </div>
+            </div>
+            <div class="col l3"> 
+              <div class="row">
+                <div class="col l6">Spell block</div>
+                <div class="col l6" id="spellblock">${championDetail.stats.spellblock}</div>
+              </div>
+            </div>
+            <div class="col l3"> 
+              <div class="row">
+                <div class="col l6">MP Regen per level</div>
+                <div class="col l6" id="mpregenperlevel">${championDetail.stats.mpregenperlevel}</div>
+              </div>
+            </div>
+            <div class="col l3"> 
+              <div class="row">
+                <div class="col l6">Attack speed per level</div>
+                <div class="col l6" id="attackspeedperlevel"> ${championDetail.stats.attackspeedperlevel}</div>
+              </div>
+            </div>
+          </div>
+        </section>
+      <div> 
+       </div>`  //cierre tarjeta card
+
+       const links = document.getElementsByClassName('link2');
+       for (let i = 0; i < links.length; i++) {
+         links[i].addEventListener('click', (event) => {
+           event.preventDefault();
+           document.getElementById("page2").style.display="block";
+           document.getElementById("page3").style.display="none";
+           showCards(data);
+         });
+       }
+   }
+   
   document.getElementById('selectRol').addEventListener("change",()=>{
-    let rol=document.getElementById('selectRol').value;
-    let data=filterByRol(datos,rol);
-    showCards(data);   
+    let condition=document.getElementById('selectRol').value;
+    let datafilter=filterData(data,condition);
+    showCards(datafilter);   
   });
 
   document.getElementById('selectOrder').addEventListener("change",()=>{
     
-    let order=document.getElementById('selectOrder').value;
-    let data=ordenar(datos,order);
-    showCards(data);
+    let sortOrder=document.getElementById('selectOrder').value;
+    let datasort=sortData(data,"id",sortOrder);
+    showCards(datasort);
   });
 
-  
+document.getElementById("estadistica").addEventListener("click",
+(event) => {
+event.preventDefault();
+
+document.getElementById("page1").style.display="none";
+document.getElementById("page2").style.display="none";
+document.getElementById("page3").style.display="none";
+document.getElementById("page4").style.display="block";
+
+
+document.getElementById('root').innerHTML=''
+const listAverage=computeStats(data);
+let image=["assassin.jpeg","fighter.jpeg","mage.jpeg","marksman.jpeg","support.jpeg","tank.jpeg"];
+for(let k=0; k<listAverage.length;k++){
+  const average=listAverage[k];  
+
+  document.getElementById("root").innerHTML+=`
+  <section class="container" style="width:100%">
+        <div class="row" >
+          <div class="col s6 m3 l3">
+            <img class="responsive-image" src="${image[k]}" > 
+          </div>
+          <div class="col l9">
+          <div class="row">
+            
+            <div   class="col s6 m3 l3" >
+              <div class="row">
+                <div class="col s12 m6 l6">Attack</div>
+                <div class="col s12 m6 l6" id="result-table">${average[0]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3" > 
+              <div class="row">
+                <div class="col s12 m6 l6">MP</div>
+                <div class="col s12 m6 l6" id="result-table">${average[6]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3" > 
+              <div class="row">
+                <div class="col s12 m6 l6">Spell block per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[12]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3" > 
+              <div class="row">
+                <div class="col s12 m6 l6">Crit</div>
+                <div class="col s12 m6 l6" id="result-table">${average[18]}</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+              
+            <div class="col s6 m3 l3">
+              <div class="row">
+                <div class="col s12 m6 l6">Defense</div>
+                <div class="col s12 m6 l6" id="result-table">${average[1]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3"> 
+              <div class="row">
+                <div class="col s12 m6 l6">MP per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[7]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3"> 
+              <div class="row">
+                <div class="col s12 m6 l6">Attack range</div>
+                <div class="col s12 m6 l6" id="result-table">${average[13]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3"> 
+              <div class="row">
+                <div class="col s12 m6 l6">Crit per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[19]}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+              
+            <div class="col s6 m3 l3" >
+              <div class="row">
+                <div class="col s12 m6 l6">Magic</div>
+                <div class="col s12 m6 l6" id="result-table">${average[2]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3" > 
+              <div class="row">
+                <div class="col s12 m6 l6">Movespeed</div>
+                <div class="col s12 m6 l6" id="result-table">${average[8]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3" > 
+              <div class="row">
+                <div class="col s12 m6 l6">HP regen</div>
+                <div class="col s12 m6 l6" id="result-table">${average[14]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3" > 
+              <div class="row">
+                <div class="col s12 m6 l6">Attack damage</div>
+                <div class="col s12 m6 l6" id="result-table">${average[20]}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+              
+            <div class="col s6 m3 l3">
+              <div class="row">
+                <div class="col s12 m6 l6">Difficulty</div>
+                <div class="col s12 m6 l6" id="result-table">${average[3]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3"> 
+              <div class="row">
+                <div class="col s12 m6 l6">Armor</div>
+                <div class="col s12 m6 l6" id="result-table">${average[9]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3"> 
+              <div class="row">
+                <div class="col s12 m6 l6">HP Regen per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[15]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3"> 
+              <div class="row">
+                <div class="col s12 m6 l6">Attack damage per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[21]}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+              
+            <div class="col s6 m3 l3" >
+              <div class="row">
+                <div class="col s12 m6 l6">HP</div>
+                <div class="col s12 m6 l6" id="result-table">${average[4]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3" > 
+              <div class="row">
+                <div class="col s12 m6 l6">Armor per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[10]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3" > 
+              <div class="row">
+                <div class="col s12 m6 l6">MP Regen per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[16]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3" > 
+              <div class="row">
+                <div class="col s12 m6 l6">Attack speed offset</div>
+                <div class="col s12 m6 l6" id="result-table">${average[22]}</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+              
+            <div class="col s6 m3 l3">
+              <div class="row">
+                <div class="col s12 m6 l6">HP per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[5]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3"> 
+              <div class="row">
+                <div class="col s12 m6 l6">Spell block</div>
+                <div class="col s12 m6 l6" id="result-table">${average[11]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3"> 
+              <div class="row">
+                <div class="col s12 m6 l6">MP Regen per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[17]}</div>
+              </div>
+            </div>
+            <div class="col s6 m3 l3"> 
+              <div class="row">
+                <div class="col s12 m6 l6">Attack speed per level</div>
+                <div class="col s12 m6 l6" id="result-table">${average[23]}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+        
+  `
+}
+
+});
+
+
+
 };
